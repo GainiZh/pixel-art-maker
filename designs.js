624c24cb3e72d00d71334c10beaff6b6e1cfaad2
GainiZh <gaini.zhulamanova@gmail.com>
2018-09-29 17:39:49 +0200

    Added erase color function

--- designs.js
+++ designs.js
@@ -3,12 +3,20 @@
 
 // When size is submitted by the user, call makeGrid()
 //set a variable for the canvas, use const instead of a var, as it was said in the style guide; use single-quotes
+const canvas = $('#pixel_canvas');
 function makeGrid() {
 //create the size of a new table by defining variables
   const height = $('#input_height').val();
   const width = $('#input_width').val();
-//create a table
-  const canvas = $('#pixel_canvas');
+//set a max. amount of rows and columns in case of higher values
+  if(height > 100) {
+  height = 100;
+  }
+  if(width > 100) {
+    width = 100;
+  }
+//delete previous canvas to create a new one
+ canvas.empty();
 //create a grid using loops
   //create rows
   for(let rows = 0; rows < height; rows++) {
@@ -31,5 +39,9 @@ function makeGrid() {
   });
  //clear data, i.e. reset button: I created an input type with the reset value in html, then created an event listener
   $('#resetButton').on('click', function(event) {
-    $('#pixel_canvas').empty();
+    canvas.empty();
   });
+//clear the color
+$("#eraseColor").click(function() {
+  $("td").css("background-color", "");
+});
